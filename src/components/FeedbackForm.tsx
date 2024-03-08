0975bfceadf2f92340fe99fc014ef6306ae26adc
Goran SD <g.cvetic23@gmail.com>
2024-03-08 13:27:39 -0800

    Refactor FeedbackForm component to include a form with textarea and submit button

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,5 +1,17 @@
 const FeedbackForm = () => {
-  return <div>FeedbackForm</div>;
+  return (
+    <form className="form">
+      <textarea id="feedback-textarea" spellCheck={false} />
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remember to #hashtag the company
+      </label>
+
+      <div>
+        <p className="u-italic">150</p>
+        <button>Submit</button>
+      </div>
+    </form>
+  );
 };
 
 export default FeedbackForm;
