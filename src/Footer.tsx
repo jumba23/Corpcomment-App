3d97945fae94f9c9fe523c9f40f1c9709ca16be0
Goran SD <g.cvetic23@gmail.com>
2024-03-08 12:19:08 -0800

    Add copyright notice and usage restriction to footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,5 +1,20 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <small>
+        <p>
+          © Copyright by{" "}
+          <a target="_blank" href="https://www.goran-portfolio.com">
+            Soft Solutions.
+          </a>
+        </p>
+        <p>
+          <span className="u-bold u-italic">Not allowed</span> to use as your
+          own teaching material.
+        </p>
+      </small>
+    </footer>
+  );
 };
 
 export default Footer;
