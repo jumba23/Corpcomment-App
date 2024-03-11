70e5a11bfe6c62080079ac329357595daa516548
Goran SD <g.cvetic23@gmail.com>
2024-03-11 09:48:18 -0700

    Add new feedback item to FeedbackList component

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,5 +1,24 @@
 const FeedbackList = () => {
-  return <ol className="feedback-list">FeedbackList</ol>;
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <span>568</span>
+        </button>
+        <div>
+          <p>B</p>
+        </div>
+        <div>
+          <p>SoftSolutions</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            laboriosam incidunt alias, libero voluptate sunt.
+          </p>
+        </div>
+        <p>4d</p>
+      </li>
+    </ol>
+  );
 };
 
 export default FeedbackList;
