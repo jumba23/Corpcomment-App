c5290597369b106decd68ed62a70a70a8ba67d60
Goran SD <g.cvetic23@gmail.com>
2024-03-11 10:02:44 -0700

    Add character count and limit to feedback textarea

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 const FeedbackForm = () => {
+  const [text, setText] = useState("");
+
+  const charCount = MAX_CHARACTERS - text.length;
+
   return (
     <form className="form">
-      <textarea id="feedback-textarea" spellCheck={false} />
+      <textarea
+        value={text}
+        onChange={(event) => {
+          const newText = event.target.value;
+          if (newText.length > MAX_CHARACTERS) return;
+          setText(newText);
+        }}
+        id="feedback-textarea"
+        spellCheck={false}
+      />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
 
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>Submit</button>
       </div>
     </form>
